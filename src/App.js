622f4003c6000c9972a0bc0ce27d7693b4fa1ac7
ramji subedi <ramjisubedi1991@gmail.com>
2022-09-03 11:43:46 +0545

    login with firebase 9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
 import Post from './components/Post';
 import Widget from './components/Widget';
 import Login from './components/Login';
+import { useSelector,useDispatch } from 'react-redux';
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth } from './firebase';
+import {loginUser,logoutUser,selectUser} from './features/userSlice'
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
+  const [loading,setLoading] = useState(true);
+useEffect(() => {
+  onAuthStateChanged(auth,(user) => {
+console.log(user)
+if(user){
+  dispatch(loginUser({
+    email: user.email,
+    uid: user.uid,
+    photoUrl:user.photoURL,
+    displayName: user.displayName
+  }))
+  setLoading(false);
+}else{
+  dispatch(logoutUser())
+  setLoading(false);
+}
+  })
+},[])
+
   return (
 <>
 {
-  !user?(<Login />) : (
-    <div className="App">
-    <Header/>
-    <div className="app__body">
-     <SideBar/>
-     <Post />
-     <Widget />
-   </div>
-   </div>
-  )
+  (!loading)?
+    !user?(<Login />) : (
+      <div className="App">
+      <Header/>
+      <div className="app__body">
+       <SideBar/>
+       <Post />
+       <Widget />
+     </div>
+     </div>
+    ):''
 }
 
    </> 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,112 @@
 import React, {useState} from 'react'
 import '../css/login.css';
+import { auth } from '../firebase';
+import {useDispatch} from 'react-redux'
+import {loginUser,logoutUser} from '../features/userSlice'
+import {createUserWithEmailAndPassword, updateProfile,signInWithEmailAndPassword} from 'firebase/auth'
+
 
 const Login = () => {
 
     const [signup, setSignup] = useState();
+    const [name, setName] = useState("");
+    const [photoURL, setPhotoURL] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch()
+
+// Register user
+    const register = async (e)=>{
+        e.preventDefault();
+        if(!name){
+            return alert("Name Required");
+        };
+        if(!photoURL){
+            return alert("Photo Required");
+        };
+        if(!email){
+            return alert("Email Required");
+        };
+        if(!password){
+            return alert("Password Required");
+        };
+        try{
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            // Signed in 
+            const users = userCredential.user;
+            updateProfile(users,{
+                displayName: name,
+                photoURL: photoURL
+                })
+                .then(() => {
+                    dispatch(loginUser({
+                        email:userCredential.user.email,
+                        uid: userCredential.user.uid,
+                        photoURL:userCredential.user.photoURL,
+                        displayName: userCredential.user.displayName,
+                    }))
+                })
+                .catch((error)=>{alert(error.message)
+            })  
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+        }catch(error){
+            const errorCode = error.code;
+            const errorMessage = error.message;        
+            }
+        }
+
+        // Login user
+
+        const login = (e)=>{
+            e.preventDefault();
+            if(!email){
+                return alert("Please enter email")
+            }
+
+            if(!password){
+                return alert("Please enter password")
+            }
+
+            try{
+                const logindetails =  signInWithEmailAndPassword(auth,email,password)
+                .then((loginUsers)=>{
+                    dispatch(loginUser({
+                        email:loginUsers.user.email,
+                        uid: loginUsers.user.uid,
+                        photoURL:loginUsers.user.photoURL,
+                        displayName: loginUsers.user.displayName,
+                    }))
+                })
+            }catch(error){
+
+            }
+        }
+
   return (
     <div className="loginScreen">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png" />
         
     {
         signup===true ? (
-<form>
-            <input type="text" placeholder="Full Name" />
-            <input type="text" placeholder="Profile URL" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+<form onSubmit={register}>
+            <input type="text" placeholder="Full Name" onChange={e=>setName(e.target.value)} />
+            <input type="text" placeholder="Profile URL" onChange={e=>setPhotoURL(e.target.value)}/>
+            <input type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
             <input type="submit" value="Signup" />
     <h4>Already a member? <span onClick={e =>setSignup(false)}>Login Here</span></h4>
         </form>
         ):(
-            <form>
+            <form onSubmit={login}>
            
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+           <input type="email" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
             <input type="submit" value="Login" />
     <h4>Not a member? <span onClick={e =>setSignup(true)}>Register Here</span></h4>
         </form>
